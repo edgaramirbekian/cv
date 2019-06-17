182e3f756c0bf91b7acf21faa9b670cf73bee7c8
Edgar <amirbekianedgar@gmail.com>
2019-06-17 17:18:50 +0400

    add "interests" page

---
+++ src/Components/Interests/Interests.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './styles.scss';
+
+function Interests() {
+    return (
+        <div className="interests">
+            <h1>Interests</h1>
+            <span className="interests-text">
+                I enjoy exploring ideas about Programming paradigms, software architecture,
+                design patterns and how things work on the low-physical level,
+                always desire to learn something new. I’m much interested in Networking and Cybersecurity,
+                a big fan of science and art. I support internet anonymity and anti-tracking.
+            </span>
+        </div>
+    );
+}
+
+export default Interests;
