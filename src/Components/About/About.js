5ee0e32df05997bb4a62b28bf25e82136a474484
Edgar <amirbekianedgar@gmail.com>
2019-06-17 17:15:48 +0400

    add "about" page

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,29 +1,45 @@
 import React from 'react';
-import './styles.css';
+import './styles.scss';
 import logo from './logo.svg';
 
 function About() {
     return (
-        <div>
-            <h1>
-                <span> EDGAR </span>
-                <br/>
-                <span> AMIRBEKYAN </span>
-            </h1>
+        <div className="about">
+            <div className="hello">
+                <h1>
+                    <span className="iam">Hi! I am</span>
+                    <br/>
+                    <span className="name"> EDGAR </span>
+                    <br/>
+                    <span className="surname"> AMIRBEKYAN </span>
+                </h1>
+            </div>
             <br/>
-                <div>
-                    <span> Armenia, Yerevan </span>
-                    <span> +374 96 071577 </span>
-                    <p> Organized and motivated software developer seeking to work closely with <br/>
-                        a team of experienced engineers to create a products which have an impact, <br/>
-                        skilled in Web Development(Ruby on Rails, NodeJS, ReactJS, Django, Flask).
-                    </p>
-                    <h6>
-                        Made with
-                        <img src={logo} className="logo_style" alt="React-logo" />
+            <div className="info">
+                <p className="letter">
+                    Organized and motivated software developer seeking to work closely with
+                    <br/>
+                    a team of experienced engineers to create a products which have an impact,
+                    <br/>
+                    skilled in Web Development(Ruby on Rails, NodeJS, ReactJS, Django, Flask).
+                </p>
+                <div className="contact">
+                    <h4>CONTACT ME</h4>
+                    <h6 className="contact-info">
+                        <span className="contact-icon fas fa-map-marker-alt"><span className="contact-text">  YEREVAN, ARMENIA  </span></span>
+                        <br/>
+                        <span className="contact-icon fas fa-phone-volume"><span className="contact-text">  +374 96 071577  </span></span>
+                        <br/>
+                        <a className="contact-icon fas fa-envelope" href='mailto:amirbekyanedgar@gmail.com'><span className="contact-text">  amirbekyanedgar@gmail.com  </span></a>
+                        <br/>
+                        <a className="contact-icon fab fa-github" href='https://github.com/edgaramirbekian'><span className="contact-text">  edgaramirbekian  </span></a>
                     </h6>
                 </div>
-
+                <h6 className="made-with">
+                    Made with
+                    <img src={logo} className="logo_style" alt="React-logo" />
+                </h6>
+            </div>
         </div>
     );
 }
