1fd8225a81df887a93d19a3750a5a62bd884b7af
Edgar <amirbekianedgar@gmail.com>
2019-06-18 18:36:58 +0400

    Updates

---
+++ precache-manifest.5d36499197306e3188b69e394a6eec9e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44910df2da61cb7d8fb94c3d114058b6",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "d4c0541a4ced355bf493",
+    "url": "/cv/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "262b227c4d4c115c7810",
+    "url": "/cv/static/css/main.ecf18159.chunk.css"
+  },
+  {
+    "revision": "d4c0541a4ced355bf493",
+    "url": "/cv/static/js/2.981f69ed.chunk.js"
+  },
+  {
+    "revision": "262b227c4d4c115c7810",
+    "url": "/cv/static/js/main.cad12a38.chunk.js"
+  },
+  {
+    "revision": "18079a4bdaee1d56978c",
+    "url": "/cv/static/js/runtime~main.6f651ed7.js"
+  },
+  {
+    "revision": "36c5ab9318386dc0499a0d89691e6331",
+    "url": "/cv/static/media/devicons.36c5ab93.eot"
+  },
+  {
+    "revision": "5ed4d6468a5a0f3cf9c82f854936feab",
+    "url": "/cv/static/media/devicons.5ed4d646.ttf"
+  },
+  {
+    "revision": "a21d3c630e856b5fb26fd3d6f1b894ff",
+    "url": "/cv/static/media/devicons.a21d3c63.svg"
+  },
+  {
+    "revision": "a8639dec5833a8375d22fe425b01f4eb",
+    "url": "/cv/static/media/devicons.a8639dec.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cv/static/media/logo.5d5d9eef.svg"
+  }
+]);
