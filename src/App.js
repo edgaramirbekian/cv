0e3755bfb8c023adc4dc3492fcc65e469f3d92ea
Edgar <amirbekianedgar@gmail.com>
2019-06-18 17:34:10 +0400

    add transition animation and router sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, NavLink } from "react-router-dom";
+import { CSSTransition } from 'react-transition-group'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Widget from './Components/Widget/Widget';
+import {Nav} from 'react-bootstrap';
 import About from './Components/About/About';
 import Education from './Components/Education/Education';
 import Experience from './Components/Experience/Experience';
@@ -14,37 +15,69 @@ import EducationData from './Info/education.json';
 import ExperienceData from './Info/experience.json';
 import SkillsData from './Info/skills.json';
 import AwardsData from './Info/awards.json';
-import './App.css';
+import './App.scss';
+
+const routes = [
+    { path: '/', name: 'About', Component: About},
+    { path: '/edu', name: 'Education', Component: Education, Data: EducationData },
+    { path: '/experience', name: 'Experience', Component: Experience, Data: ExperienceData },
+    { path: '/skills', name: 'Skills', Component: Skills, Data: SkillsData },
+    { path: '/interests', name: 'Interests', Component: Interests },
+    { path: '/awards', name: 'Awards', Component: Awards, Data: AwardsData }
+];
 
 function App() {
   return (
-      <Container id="main-cont">
-          <Row>
-              <Col sm={4}>
-                  <Widget />
-              </Col>
-              <Col sm={8}>
-                  <div className="wrap-content">
-                      <BrowserRouter>
-                          <Switch>
-                              <Route exact path="/"
-                                 component={About} />
-                              <Route path="/experience"
-                                 render={(props) => <Experience {...props} data={ExperienceData} />} />
-                              <Route path="/edu"
-                                 render={(props) => <Education {...props} data={EducationData} />} />
-                              <Route path="/skills"
-                                 render={(props) => <Skills {...props} data={SkillsData} />} />
-                              <Route path="/interests"
-                                 component={Interests} />
-                              <Route path="/awards"
-                                 render={(props) => <Awards {...props} data={AwardsData} />} />
-                          </Switch>
-                      </BrowserRouter>
-                  </div>
-              </Col>
-        </Row>
-    </Container>
+      <BrowserRouter>
+          <Container id="main-cont">
+              <Row>
+                  <Col sm={4}>
+                      <main className="main">
+                          <aside className="sidebar">
+                              <nav className="nav">
+                                  {routes.map(route => (
+                                      <ul>
+                                          <li>
+                                              <Nav.Link
+                                                  key={route.path}
+                                                  as={NavLink}
+                                                  to={route.path}
+                                                  activeClassName="active"
+                                                  exact
+                                              >
+                                                  {route.name}
+                                              </Nav.Link>
+                                          </li>
+                                      </ul>
+                                  ))}
+                              </nav>
+                          </aside>
+                      </main>
+                  </Col>
+                  <Col sm={8} >
+                      {/*<Switch>*/}
+                      {routes.map(({ path, Component, Data }) => (
+                          <Route key={path} exact path={path}>
+                              {({ match }) => (
+                                  <CSSTransition
+                                      in={match != null}
+                                      timeout={300}
+                                      classNames="page"
+                                      unmountOnExit
+                                  >
+                                      <div className="page">
+                                          <Component data={Data!==undefined ? Data : null}/>
+                                      </div>
+                                  </CSSTransition>
+                              )}
+                          </Route>
+                      ))}
+                      {/*</Switch>*/}
+                  </Col>
+            </Row>
+        </Container>
+      </BrowserRouter>
+
   );
 }
 
