eeec871c9da19d115cb8089e1e1ae3b5af848af4
Edgar <amirbekianedgar@gmail.com>
2019-06-18 19:47:42 +0400

    comment service workers

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 import Boostrap from 'bootstrap/dist/css/bootstrap.css';
 import Favicon from 'react-favicon';
 
@@ -18,7 +18,7 @@ RenderApp(App);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
 
 // Webpack Hot Module Replacement API
 
